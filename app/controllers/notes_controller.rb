e1638d071b2a44d56ba67d1663efe90a722e1255
mioishiguro <mmmochaio@gmail.com>
2017-01-23 18:06:38 +0900

    削除機能

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,4 +17,22 @@ class NotesController < ApplicationController
   def show
     @note = Note.find(params[:id])
   end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note =Note.find(params[:id])
+    @note.title = params[:title]
+    @note.content = params[:content]
+    @note.save
+    redirect_to note_path(@note.id)
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path
+  end
 end
