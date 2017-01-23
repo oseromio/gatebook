06c556b4c11d84916a208281162ae022ace8d005
mioishiguro <mmmochaio@gmail.com>
2017-01-23 16:57:42 +0900

    new index追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'notes/new' => 'notes#new'
+  post '/notes' => 'notes#create'
+  get '/notes' => 'notes#index'
+  get '/notes/:id' => 'notes#show', as:'note'
   root 'home#top'
   get '/about' => 'home#about'
+
 end
